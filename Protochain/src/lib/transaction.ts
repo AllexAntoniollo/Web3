35f03f4977193dde8ce344e32e08bcbfe7a4943e
AllexAntoniollo <allexatoniollo@gmail.com>
2023-09-17 09:10:05 -0300

    Refresh

---
+++ Protochain/src/lib/transaction.ts
@@ -0,0 +1,39 @@
+import TransactionType from "./transactionType";
+import { SHA256 } from "crypto-js";
+import Validation from "./validation";
+import TransactionInput from "./transactionInput";
+
+/**
+ * Transaction Class
+ */
+export default class Transaction{
+    type: TransactionType;
+    timestamp: number
+    hash : string
+    to : string
+    txInput : TransactionInput
+
+    constructor(tx?:Transaction) {
+        this.type = tx?.type || TransactionType.REGULAR
+        this.to = tx?.to || ""
+        this.timestamp = tx?.timestamp || Date.now()
+        this.hash = tx?.hash || this.getHash()
+        this.txInput = new TransactionInput(tx?.txInput) || new TransactionInput()
+    }
+
+    getHash() : string{
+        return SHA256(this.type+this.to+this.timestamp+this.txInput.getHash()).toString()
+    }
+
+    isValid() : Validation{
+        if(this.hash !== this.getHash())
+            return new Validation(false,"Invalid Hash.")
+
+        if(!this.to)
+              return new Validation(false,"Invalid to.")
+
+        return new Validation()
+    }
+
+
+}
