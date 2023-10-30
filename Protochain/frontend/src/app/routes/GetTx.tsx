581178b02d44ab713df55da34b8e72df0062c5c1
AllexAntoniollo <allexatoniollo@gmail.com>
2023-10-30 14:05:35 -0300

    Code optimization.

--- Protochain/frontend/src/app/routes/GetTx.tsx
+++ Protochain/frontend/src/app/routes/GetTx.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import '../style/GetTx.css'
 import { Link } from 'react-router-dom'
 import { BiArrowBack } from "react-icons/bi";
 
--- Protochain/frontend/src/app/routes/SendTx.tsx
+++ Protochain/frontend/src/app/routes/SendTx.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import '../style/SendTx.css'
 import { Link } from 'react-router-dom'
 import { BiArrowBack } from "react-icons/bi";
 
